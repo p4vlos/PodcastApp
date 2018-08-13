ba6e9e2cecd35a781c7f1a10c6d4f88e67708dad
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-08-13 21:19:54 +0100

    added Alamofire with pods and parsing JSON from iTunes

--- PodcastLBTA/Controllers/PodcastsSearchController.swift
+++ PodcastLBTA/Controllers/PodcastsSearchController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
     
@@ -39,6 +40,18 @@ class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
         //later implement Alamofire to search iTunes API
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        Alamofire.request(url).responseData { (dataResponse) in
+            if let err = dataResponse.error {
+                print("Failed to contact yahoo ", err)
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+            let dummyString = String(data: data, encoding: .utf8)
+            print(dummyString ?? "")
+        }
+        
     }
     
     fileprivate func setupTableView() {
---
+++ Pods/Target Support Files/Pods-PodcastLBTA/Pods-PodcastLBTA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodcastLBTA : NSObject
+@end
+@implementation PodsDummy_Pods_PodcastLBTA
+@end
