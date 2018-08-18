85b8a202d1856755e8a3629ddc07f37448c50f89
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-08-18 15:20:55 +0100

    connected the nib file with PodcastCell swift file

--- PodcastLBTA/Views/PodcastCell.swift
+++ PodcastLBTA/Views/PodcastCell.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class PodcastCell: UITableViewCell {
+    @IBOutlet weak var podcastImageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var episodeCountLabel: UILabel!
+    
+    var podcast: Podcast!
     
 }
