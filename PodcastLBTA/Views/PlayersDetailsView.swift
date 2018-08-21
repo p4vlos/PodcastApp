f91d3514be9e89fe1293bded6b1a6db3dbed0db2
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-08-21 17:02:29 +0100

    playing a podcast episode

--- PodcastLBTA/Views/PlayersDetailsView.swift
+++ PodcastLBTA/Views/PlayersDetailsView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class PlayerDetailsView: UIView {
     
@@ -15,14 +16,31 @@ class PlayerDetailsView: UIView {
             titleLabel.text = episode.title
             authorLabel.text = episode.author
             
+            playEpisode()
+            
             guard let url = URL(string: episode.imageUrl ?? "") else { return }
             episodeImageView.sd_setImage(with: url)
         }
     }
     
+    fileprivate func playEpisode() {
+        print("Trying to play episode at url: ", episode.streamUrl)
+        
+        guard let url = URL(string: episode.streamUrl) else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
+    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        return avPlayer
+    }()
+    
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview()
     }
+    
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var episodeImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel! {
