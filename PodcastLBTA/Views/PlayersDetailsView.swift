f097a4b75047ae875701cf9b188a98cf2998e043
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2018-08-22 06:05:06 +0100

    update current time, duration labels and current time slider

--- PodcastLBTA/Views/PlayersDetailsView.swift
+++ PodcastLBTA/Views/PlayersDetailsView.swift
@@ -38,11 +38,31 @@ class PlayerDetailsView: UIView {
         return avPlayer
     }()
     
+    fileprivate func observerPlayerCurrentTime() {
+        let interval = CMTimeMake(1, 2)
+        player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { (time) in
+            self.currentTimeLabel.text = time.toDisplayString()
+            let durationTime = self.player.currentItem?.duration
+            self.durationLabel.text = durationTime?.toDisplayString()
+            
+            self.updateCurrentTimeSlider()
+        }
+    }
+    
+    fileprivate func updateCurrentTimeSlider() {
+        let currentTimeSeconds = CMTimeGetSeconds(player.currentTime())
+        let durationSeconds = CMTimeGetSeconds(player.currentItem?.duration ?? CMTimeMake(1, 1))
+        let percentage = currentTimeSeconds / durationSeconds
+        
+        self.currentTimeSlider.value = Float(percentage)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        let time = CMTimeMake(1, 3)
+        observerPlayerCurrentTime()
         
+        let time = CMTimeMake(1, 3)
         let times = [NSValue(time: time)]
         player.addBoundaryTimeObserver(forTimes: times, queue: .main) {
             self.enlargeEpisodeImageView()
@@ -51,6 +71,9 @@ class PlayerDetailsView: UIView {
     
     //MARK:- IB Actions and Outlets
     
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var currentTimeLabel: UILabel!
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview()
     }
